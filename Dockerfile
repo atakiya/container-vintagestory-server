f726e087d2ad16a0e2422b0be3515dcb72f71de6
Alex 'Avunia' Takiya <git@takiya.eu>
2024-08-10 19:23:10 +0200

    Fix container image, md5 check downloads, pull data via API
    Git ate my COMMIT_EDITMSG and i'm too tired to retype everything.
    but tl;dr $SERVER_VERSION is defaulted to 'latest' now and will try to download a newer version at every boot.
    Download URLs, MD5 hashes and such are now gathered via API as well
    If API data cant be pulled, server will try to start a set version if not latest

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,28 @@
-FROM mcr.microsoft.com/dotnet/runtime:7.0-alpine AS base
+FROM mcr.microsoft.com/dotnet/runtime:7.0 AS base
 
-ENV USER "vintagestory"
-ENV UID "1001"
-ENV GID "1001"
-ENV SERVER_VERSION "1.18.8"
+LABEL org.opencontainers.image.authors="Avunia Takiya <avunia.takiya.eu>"
+LABEL org.opencontainers.image.licenses="MIT"
+LABEL org.opencontainers.image.source="https://github.com/atakiya/container-vintagestory-server/"
 
-RUN apk update \
-	&& apk add --no-cache \
-	ca-certificates \
-	gcompat
+ENV USER="vintagestory"
+ENV UID="1001"
+ENV GID="1001"
+ENV SERVER_VERSION="latest"
+ENV UPDATE_CHANNEL="stable"
+
+ARG DEBIAN_FRONTNED=noninteractive
+
+RUN apt-get update \
+	&& apt-get upgrade -y \
+	&& apt-get dist-upgrade -y \
+	&& apt-get install -y --no-install-recommends \
+	wget jq
+
+FROM base AS app
 
 # Setup service user
-RUN addgroup --gid ${GID} -S ${USER} && \
-	adduser -G ${USER} --shell /bin/false --disabled-password -H --uid ${UID} ${USER} && \
+RUN addgroup --gid ${GID} --system ${USER} && \
+	adduser --ingroup ${USER} --shell /bin/false --disabled-password --no-create-home --uid ${UID} ${USER} && \
 	mkdir -p /var/log/${USER} && \
 	chown ${USER}:${USER} /var/log/${USER}
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,65 @@
 #!/bin/sh
 
-PLATFORM=linux
-ARCH=x64
+API_URL=https://api.vintagestory.at/stable.json
+SERVER_VERSION="${SERVER_VERSION:-latest}"
 
-BOOTSTRAP_URL=https://cdn.vintagestory.at/gamefiles/stable/vs_server_${PLATFORM}-${ARCH}_${SERVER_VERSION}.tar.gz
+bs_cd () { cd "$@" || exit; }
+bs_print () { printf '%s\n' "[BOOTSTRAP] ""$*"""; }
 
-bs_print () { printf '%s\n' "[BOOTSTRAP] ""$@"""; }
+command -v wget >/dev/null 2>&1 || { bs_print "FATAL: wget can not be executed." >&2; exit 1; }
+command -v jq >/dev/null 2>&1 || { bs_print "FATAL: jq can not be executed." >&2; exit 1; }
+bs_cd /app
 
-command -v wget >/dev/null 2>&1 || { bs_print "Fatal Error: wget can not be executed." >&2; exit 1; }
+_API_DATA=$(wget -O- $API_URL)
 
-cd /app
+if [ -n "$_API_DATA" ]; then
+	if [ "$SERVER_VERSION" = "latest" ]; then
+		_VERSION_DATA=$(jq -n --argjson data "$_API_DATA" '$data | to_entries | map(select(.value.linuxserver.latest)) | .[0]')
+	else
+		_VERSION_DATA=$(jq -n --argjson data "$_API_DATA" --arg version "$SERVER_VERSION" '$data | to_entries | map(select(.key == $version)) | .[0]')
+	fi
+
+	_VERSION=$(jq -n -r --argjson data "$_VERSION_DATA" '$data.key')
+	_SERVERDATA=$(jq -n --argjson data "$_VERSION_DATA" '$data.value.linuxserver // $data.value.server')
+
+	_MD5HASH=$(jq -n -r --argjson data "$_SERVERDATA" '$data.md5')
+	_URL_CDN=$(jq -n -r --argjson data "$_SERVERDATA" '$data.urls.cdn')
+	_URL_MIRROR=$(jq -n -r --argjson data "$_SERVERDATA" '$data.urls.local')
+
+	_APP_DIR="/app/${_VERSION}"
+else
+	bs_print "WARN: API server not reachable"
+	# $SERVER_VERSION is set at this point, so we use that instead
+	_APP_DIR="/app/${SERVER_VERSION}"
+fi
 
 bs_print "Checking if we need to update the server..."
-if [ ! -d "/app/${SERVER_VERSION}" ] || [ ! "$(ls -A /app/${SERVER_VERSION})" ]; then
-	bs_print "Version ${SERVER_VERSION} not installed, downloading..."
-	mkdir -p /app/${SERVER_VERSION}
-	wget -O- "${BOOTSTRAP_URL}" | tar xz --directory="/app/${SERVER_VERSION}"
+if [ ! -d "$_APP_DIR" ] || [ ! "$(ls -A "$_APP_DIR")" ]; then
+	if [ -z "$_API_DATA" ]; then
+		bs_print "FATAL: Version ${SERVER_VERSION} not installed, API server not reachable, cannot run server, exiting!" >&2
+		exit 1
+	fi
+
+	bs_print "Version ${_VERSION} not installed, downloading..."
+
+	mkdir -p "$_APP_DIR"
+	_TMPDLFILE=$(mktemp --tmpdir "vs-${_VERSION}.XXXXXXXXXX")
+
+	if ! wget -O "$_TMPDLFILE" "$_URL_CDN"; then
+		bs_print "Downloading from CDN failed, trying mirror..."
+		wget -O "$_TMPDLFILE" "$_URL_MIRROR"
+	fi
+
+	bs_print "Checking file hash..."
+	echo "$_MD5HASH" "$_TMPDLFILE" | md5sum --check -
+
+	bs_print "Unpacking archive..."
+	tar xvf "$_TMPDLFILE" --directory="$_APP_DIR"
+	bs_print "Cleaning up after ourselves..."
+	rm -f "$_TMPDLFILE"
 fi
 
 bs_print "Handing over to app..."
-cd /app/${SERVER_VERSION}
+bs_cd "$_APP_DIR"
 
 exec dotnet ./VintagestoryServer.dll --dataPath /data "$@"
